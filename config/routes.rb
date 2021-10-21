d29608662cfc05d9556cf7546a11c48fc645c696
joan blanco <joanblanco1906@gmail.com>
2021-10-21 10:12:36 -0400

    add review

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'restaurants#index'
   resources :restaurants, only: [:index, :show, :new, :create] do
     resources :reviews, only: [:new, :create]
   end
